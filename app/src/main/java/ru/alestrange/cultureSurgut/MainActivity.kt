d79b06a65371842756d1dff1ac1e332f4599ca65
Alexander Strange <yungofgimn@gmail.com>
2021-11-14 20:57:18 +0500

    2

--- app/src/main/java/ru/alestrange/cultureSurgut/MainActivity.kt
+++ app/src/main/java/ru/alestrange/cultureSurgut/MainActivity.kt
@@ -2,13 +2,17 @@ package ru.alestrange.cultureSurgut
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.room.Room
+import android.view.View
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val db= Room.databaseBuilder(applicationContext, SurgutCultureDatabase::class.java, "surgut-culture-database")
-//        db.build()
+    }
+    fun button4(view: View) {
+        // val myToast = Toast.makeText(this, message, duration);
+        val randomIntent = Intent(this, SecondActivity::class.java)
+        startActivity(randomIntent)
     }
 }
