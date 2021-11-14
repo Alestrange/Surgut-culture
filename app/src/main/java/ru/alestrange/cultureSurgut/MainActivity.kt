c71da5322dae720e82fb6754f3d06dd7650dfa9f
Alexander Strange <yungofgimn@gmail.com>
2021-11-14 19:51:30 +0500

    ddd

--- app/src/main/java/ru/alestrange/cultureSurgut/MainActivity.kt
+++ app/src/main/java/ru/alestrange/cultureSurgut/MainActivity.kt
@@ -2,10 +2,17 @@ package ru.alestrange.cultureSurgut
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun button4(view: View) {
+        // val myToast = Toast.makeText(this, message, duration);
+        val randomIntent = Intent(this, SecondActivity::class.java)
+        startActivity(randomIntent)
     }
 }
