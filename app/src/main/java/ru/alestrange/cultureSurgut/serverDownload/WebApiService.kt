f26401cedd2e0e4b0facbb0b15d635496e7a1410
Sergey Yung <ivanop@yandex.ru>
2022-04-19 22:12:10 +0500

    1`

--- app/src/main/java/ru/alestrange/cultureSurgut/serverDownload/WebApiService.kt
+++ app/src/main/java/ru/alestrange/cultureSurgut/serverDownload/WebApiService.kt
@@ -2,12 +2,13 @@ package ru.alestrange.cultureSurgut.serverDownload
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import okhttp3.internal.Version
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import ru.alestrange.cultureSurgut.data.*
+
 
 private const val BASE_URL =
     "https://raw.githubusercontent.com/Alestrange/Surgut-culture/master/app/src/main/res/raw/"
@@ -16,8 +17,13 @@ private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
 
+var okHttpClient = OkHttpClient.Builder()
+    .cache(null)
+    .build()
+
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .client(okHttpClient)
     .baseUrl(BASE_URL)
     .build()
 
